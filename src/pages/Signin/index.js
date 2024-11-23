9d271f2d451bb7d6920d88d1510f89dfdb764637
Abilio <abiliogamanetojunior@gmail.com>
2024-11-22 21:05:05 -0300

    First commit

---
+++ src/pages/Signin/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import * as C from "./styles";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (!email | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    const res = signin(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/home");
+  };
+
+  return (
+    <C.Container>
+      <C.Label>LOGIN</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Entrar" onClick={handleLogin} />
+      </C.Content>
+    </C.Container>
+  );
+};
+
+export default Signin;
