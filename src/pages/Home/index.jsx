62b9689540f658aeb8644c4a7767fd83cd8a1413
Abilio <abiliogamanetojunior@gmail.com>
2024-11-24 17:01:06 -0300

    Mudanças na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,118 @@
-import React from "react";
-// import { useNavigate } from "react-router-dom";
+import React,{useState, useEffect} from 'react'
 import Navbar from "../../components/Navbar";
-import { Main, Container } from "./styles"
 // import Button from "../../components/Button";
 // import useAuth from "../../hooks/useAuth";
+// import { useNavigate } from "react-router-dom";
 // // import * as C from "./styles";
 
+import { View } from '../../components/View';
+import { 
+  MainPage, 
+  Container, 
+  Wrapper, 
+  Title,  
+  FormContainer, 
+  ViewContainer, 
+  FormGroup, 
+  Button, 
+  Table, 
+  TableResponsive 
+} from "./styles";
+
+const getDatafromLS = () => {
+  const data = localStorage.getItem('pontos');
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
+
 const Home = () => {
-  // const { signout } = useAuth();
-  // const navigate = useNavigate();
+  const [pontos, setPontos] = useState(getDatafromLS());
+
+  const [Justificativa, setJustificativa] = useState('');
+  const [Data, setData] = useState('');
+  const [Hora, setHora] = useState('');
+
+  const handleAddpontoubmit = (e) => {
+    e.preventDefault();
+    let ponto = { Justificativa, Data, Hora };
+    setPontos([...pontos, ponto]);
+    setJustificativa('');
+    setData('');
+    setHora('');
+  };
+
+  useEffect(() => {
+    localStorage.setItem('pontos', JSON.stringify(pontos));
+  }, [pontos]);
 
   return (
-
-     <Main>
+    <MainPage>
       <Navbar />
       <Container>
+        <Wrapper>
+          <Title>Ponto Senai</Title>
+          <div>
+            <FormContainer>
+              <FormGroup autoComplete="off" onSubmit={handleAddpontoubmit}>
+              {/* <label>Justificativa</label>
+                <input 
+                  type="text" 
+                  required 
+                  onChange={(e) => setJustificativa(e.target.value)} 
+                  value={Justificativa} 
+                />
+                <br />
+              <label>Data </label>
+                <input 
+                  type='date' 
+                  required 
+                  onChange={(e) => setData(e.target.value)} 
+                  value={Data} 
+                />
+                <br />
+              <label>Hora</label>
+                <input 
+                  type="time" 
+                  required 
+                  onChange={(e) => setHora(e.target.value)} 
+                  value={Hora} 
+                />
+                <br /> */}
+                <Button type="submit">Bater Ponto</Button>
+              </FormGroup>
+            </FormContainer>
+
+            <ViewContainer>
+              {pontos.length > 0 ? (
+                <>
+                  <TableResponsive>
+                    <Table>
+                      <thead>
+                        <tr>
+                          <th>Justificativa</th>
+                          <th>Data</th>
+                          <th>Hora</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                          <View pontos={pontos} />
+                        </tbody>
+                    </Table>
+                    
+                  </TableResponsive>
+                  
+                </>
+              ) : (
+                <div>Nenhum ponto foi adicionado até o momento</div>
+              )}
+            </ViewContainer>
+          </div>
+        </Wrapper>
       </Container>
-      </Main>
+    </MainPage>
   );
 };
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Main = styled.div`
+export const MainPage = styled.div`
   width: 100%;
   height: 100%;
 `;
@@ -16,5 +16,84 @@ export const Container = styled.div`
   width:60vw;
   gap: 20px;
 `;
+export const Wrapper = styled.div`
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
 
-export const Title = styled.h2``;
+export const Title = styled.h1`
+  margin-top: 30px;
+  text-align: center;
+`;
+
+export const Paragraph = styled.p`
+  text-align: center;
+  margin-top: 7px;
+  font-weight: bold;
+`;
+
+export const FormContainer = styled.div`
+  width: 500px;
+  height: 75px;
+  background-color: #e4e4e4;
+  margin: 20px auto;
+  padding: 20px;
+`;
+
+export const FormGroup = styled.form`
+  display: flex;
+  flex-direction: column;
+  label {
+    margin-bottom: 5px;
+  }
+  input {
+    margin-bottom: 15px;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: ${(props) => (props.danger ? "#dc3545" : "#046ee5")};
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: ${(props) => (props.danger ? "#c82333" : "#087cff")};
+  }
+`;
+
+export const ViewContainer = styled.div`
+  min-width: 600px;
+  background-color: #e4e4e4;
+  margin: 20px auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TableResponsive = styled.div`
+  overflow-x: auto;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+
+  th {
+    background-color: #f4f4f4;
+    text-align: left;
+  }
+`;
+
